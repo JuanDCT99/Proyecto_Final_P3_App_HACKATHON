7f25aef1718c6b05aea2aa895a8d1b90d74964dd
Camilo2B <camilo.ruizl@uqvirtual.edu.co>
2025-11-16 12:38:02 -0500

    merge 3

---
+++ proyecto_final/lib/proyecto_final/application.ex
@@ -0,0 +1,21 @@
+defmodule ProyectoFinal.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: ProyectoFinal.Worker.start_link(arg)
+      # {ProyectoFinal.Worker, arg}
+      {ProyectoFinal.Chat.Server, []}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ProyectoFinal.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
